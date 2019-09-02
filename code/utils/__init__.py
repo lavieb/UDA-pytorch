e0c5430ca22bda49f7c43468fd98ea5b291ca38a
lavieb <benjamin.lavie@etu.enseeiht.fr>
2019-09-02 15:20:54 +0200

    Remove cifar files

--- code/utils/__init__.py
+++ code/utils/__init__.py
@@ -6,10 +6,6 @@ import torch
 from torchvision import models
 from torchvision.transforms import Compose, ToTensor, Normalize, Pad, RandomCrop, RandomHorizontalFlip
 
-from cifar.datasets import get_train_test_datasets as get_cifar_train_test_datasets
-from cifar import fastresnet
-from cifar import wideresnet
-
 from utils import autoaugment
 from utils.transforms import RandomErasing, ToTensor
 
@@ -23,61 +19,6 @@ def set_seed(seed):
     random.seed(seed)
     torch.manual_seed(seed)
     np.random.seed(seed)
-
-
-def get_train_test_loaders(dataset_name, path, num_labelled_samples, batch_size, num_workers, unlabelled_batch_size=None, pin_memory=True):
-
-    if "cifar" in dataset_name.lower():
-        train_ds, test_ds, num_classes = get_cifar_train_test_datasets(dataset_name, path)
-    else:
-        raise RuntimeError("Unknown dataset '{}'".format(dataset_name))
-
-    train_labelled_ds, train_unlabelled_ds = \
-        stratified_train_labelled_unlabelled_split(train_ds, 
-                                                   num_labelled_samples=num_labelled_samples, 
-                                                   num_classes=num_classes, seed=12)
-
-    if "cifar" in dataset_name.lower():
-        train_transform = Compose([
-            Pad(4),
-            RandomCrop(32, fill=128),
-            RandomHorizontalFlip(),
-            ToTensor(),
-            Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-            RandomErasing(scale=(0.1, 0.33)),
-        ])
-
-        test_transform = Compose([
-            ToTensor(),
-            Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-        ])
-
-        unsupervised_train_transformation = Compose([
-            Pad(4),
-            RandomCrop(32, fill=128),
-            autoaugment.CIFAR10Policy(),
-            ToTensor(),
-            Normalize((0.485, 0.456, 0.406), (0.229, 0.224, 0.225)),
-            RandomErasing(scale=(0.1, 0.33)),
-        ])
-
-        train_labelled_ds = TransformedDataset(train_labelled_ds, 
-                                               transform_fn=lambda dp: (train_transform(dp[0]), dp[1]))
-        test_ds = TransformedDataset(test_ds, transform_fn=lambda dp: (test_transform(dp[0]), dp[1]))
-
-        original_transform = lambda dp: train_transform(dp[0])
-        augmentation_transform = lambda dp: unsupervised_train_transformation(dp[0])
-        train_unlabelled_ds = TransformedDataset(train_unlabelled_ds, UDATransform(original_transform, augmentation_transform))
-
-    if unlabelled_batch_size is None:
-        unlabelled_batch_size = batch_size
-
-    train_labelled_loader = DataLoader(train_labelled_ds, batch_size=batch_size, num_workers=num_workers, pin_memory=pin_memory)
-    train_unlabelled_loader = DataLoader(train_unlabelled_ds, batch_size=unlabelled_batch_size, num_workers=num_workers, pin_memory=pin_memory)
-
-    test_loader = DataLoader(test_ds, batch_size=batch_size * 2, num_workers=num_workers, pin_memory=pin_memory)
-
-    return train_labelled_loader, train_unlabelled_loader, test_loader
 
 
 def get_uda_train_test_loaders(train_set,
